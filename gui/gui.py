52b31ecc1702bfcca65c52feb97c1dd5e157fb5f
CynCyan <sullivank161@strose.edu>
2021-01-19 18:27:49 -0500

    Added buttons to convert text to upper or lower added copy and paste button to output box

--- gui/gui.py
+++ gui/gui.py
@@ -26,9 +26,44 @@ def genEmotes(lb1, lb2, e1, e2, b1):
 def printEmotes(message):
     outputBox = Tk()
     outputBox.title("output")
+
+    # create exit button at top of print
+    menubar = Menu(outputBox)
+    menubar.add_command(label="Close Window", command=lambda: outputBox.destroy())
+    outputBox.config(menu=menubar)
+
+    # button to copy to clipboard
+    menubar.add_command(label="Copy to Clipboard", command=lambda: clipit(message))
+
     outText = Text(outputBox, width=100)
     outText.insert(END, message)
     outText.grid(row=0)
+
+
+# copies message to clipboard
+def clipit(message):
+    c = Tk()
+    c.withdraw()
+    c.clipboard_clear()
+    c.clipboard_append(message)
+    c.update()
+    c.destroy()
+
+
+# converts message in entry box to uppercase
+def upperbox(e2):
+    newmessage = e2.get("1.0", END)
+    newmessage = str.upper(newmessage)
+    e2.delete("1.0", END)
+    e2.insert("1.0", newmessage)
+
+
+# converts message in entry box to lowerbox
+def lowerbox(e2):
+    newmessage = e2.get("1.0", END)
+    newmessage = str.lower(newmessage)
+    e2.delete("1.0", END)
+    e2.insert("1.0", newmessage)
 
 
 # The GUI, opens a window with text boxes to enter in the prefix and message
@@ -56,6 +91,12 @@ def main():
     startText = "use # to specify the letter location in a prefix, enter the message here. emotes are case sensitive."
     ENTR2.insert("1.0", startText)
 
+    # buttons to convert entry text to upper or lower
+    butto2 = Button(root, text="Make Text Uppercase", font=('Arial', 14),
+                    command=lambda: upperbox(ENTR2))
+    butto3 = Button(root, text="Make Text Lowercase", font=('Arial', 14),
+                    command=lambda: lowerbox(ENTR2))
+
     # final button that generates the emotes
     butto1 = Button(root, text="Output text", font=('Arial', 20),
                     command=lambda: genEmotes(LB1, LB2, ENTR1, ENTR2, butto1))
@@ -65,7 +106,9 @@ def main():
     ENTR1.grid(row=2, column=0, columnspan=3)
     LB2.grid(row=3, column=0, columnspan=3)
     ENTR2.grid(row=4, column=0, columnspan=3)
-    butto1.grid(row=5, column=0, columnspan=3)
+    butto2.grid(row=5, column=0)
+    butto3.grid(row=5, column=2)
+    butto1.grid(row=6, column=0, columnspan=3)
 
     root.mainloop()
 
